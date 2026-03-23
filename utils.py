c42a875c2bef6b4bc7b3197a6247eb7866ea097e
deepakbpit2504 <dudejadeepak22@gmail.com>
2026-03-23 22:00:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def create_time_slots(start, end, duration):
     current = start
 
     while current < end:
-        total = current.hour * 60 + current.minute + duration
+        total = current.hour*60 + current.minute + duration
         h = total // 60
         m = total % 60
 
@@ -21,15 +21,13 @@ def export_to_excel(tt, faculty_tt):
 
     writer = pd.ExcelWriter(output, engine='openpyxl')
 
-    # Section-wise sheets
     for sec, df in tt.items():
         df.to_excel(writer, sheet_name=f"Section_{sec}")
 
-    # Faculty sheet
-    faculty_df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in faculty_tt.items()]))
+    faculty_df = pd.DataFrame(dict([(k, pd.Series(v)) for k,v in faculty_tt.items()]))
     faculty_df.to_excel(writer, sheet_name="Faculty")
 
-    writer.close()  # VERY IMPORTANT
-
+    writer.close()
     output.seek(0)
+
     return output
